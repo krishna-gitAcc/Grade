8a0cc37a90b5fe79f77dc8c0dbdff7794c416ad4
krishna-creater <krish_n_a@outlook.com>
2022-09-14 15:27:16 +0530

    spi is calculated and displayed to console and the pages as well

--- src/Components/Grade.js
+++ src/Components/Grade.js
@@ -24,26 +24,28 @@ export const Grade = (props) => {
   // }
 
 
-  let spi = 0;
+  const [spi, setSpi] = useState(null)
   let gradeProvided = false;
-  const calculateSPI = () =>{
+  const calculateSPI = () => {
     // console.log(grade);
     let ttl_credit = 0;
     let ttl_marks = 0;
     for (const key in grade) {
-      if(grade[key] ==='grade'){
+      if (grade[key] === 'grade') {
         alert(`Enter A Valid grade for course  ${key}`);
         gradeProvided = false;
         return;
       }
-      const particular_course = course.find(element =>element.course===key);
+      const particular_course = course.find(element => element.course === key);
       console.log(particular_course.credit, grade[key]);
-      ttl_credit= ttl_credit+particular_course.credit;
-      ttl_marks = ttl_marks + particular_course.credit*grade[key];
+      ttl_credit = ttl_credit + particular_course.credit;
+      ttl_marks = ttl_marks + particular_course.credit * grade[key];
     }
     console.log(ttl_credit, ttl_marks);
-    spi = ttl_marks/ttl_credit;
-    console.log(spi);
+    let curr_spi = ttl_marks / ttl_credit;
+
+    setSpi(curr_spi.toFixed(2));
+    // console.log(spi);
     gradeProvided = true;
   }
 
@@ -58,7 +60,7 @@ export const Grade = (props) => {
         </thead>
         <tbody>{
           course.map((element) => {
-            return <Element key={element.course} courseName={element.course} setGrade = {setGrade} grade={grade} />
+            return <Element key={element.course} courseName={element.course} setGrade={setGrade} grade={grade} />
           })}
         </tbody>
       </table>
@@ -66,9 +68,10 @@ export const Grade = (props) => {
       <button type="button" className="btn btn-outline-dark btn-lg" onClick={calculateSPI}>Calculate SPI</button>
 
     </div>
-    <div className="container">
-      
-    </div>
+      <div className="container">
+        {spi && <h1>Your Spi is {spi}</h1>}
+      </div>
+
 
 
     </>
